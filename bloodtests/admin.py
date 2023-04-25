2d12230072570e68eeecd90a26115fe2173c3e28
Andrewsx <andrewsopoku6@gmail.com>
2023-04-25 16:18:25 +0100

    comment out unused import

--- bloodtests/admin.py
+++ bloodtests/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Test, TestResult
+# from .models import Test
+#
+# admin.site.register(Test)
 
-admin.site.register(Test)
-admin.site.register(TestResult)
