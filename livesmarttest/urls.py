b9a68ac34598b853dc5ada2dd562b2a431eb5141
Andrewsx <andrewsopoku6@gmail.com>
2023-04-25 18:48:51 +0100

    Swagger url added to path for api documentation

--- livesmarttest/urls.py
+++ livesmarttest/urls.py
@@ -1,8 +1,12 @@
+from django.conf import settings
 from django.conf.urls import include
 from django.urls import path
 from django.contrib import admin
+from swagger_schema import SwaggerTestSchemaView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/bloodtests/', include('bloodtests.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns.append(path('api/docs/', SwaggerTestSchemaView.as_view()))
