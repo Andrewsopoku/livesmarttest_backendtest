fa7dc169a7bd1ba5bd21f29656bfb77a9178ac62
Andrewsx <andrewsopoku6@gmail.com>
2023-04-25 18:36:24 +0100

    drf exception handler updated

--- exceptions.py
+++ exceptions.py
@@ -1,10 +1,14 @@
 from rest_framework.views import exception_handler
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError
 
 
 def custom_drf_exception_handler(exc, context):
-    print (exc, context)
+    # Update detail for validation exceptions
+    if isinstance(exc, ValidationError):
+        if 'non_field_errors' in exc.detail:
+            exc.detail = exc.detail['non_field_errors']
     response = exception_handler(exc, context)
     if response:
         return response
